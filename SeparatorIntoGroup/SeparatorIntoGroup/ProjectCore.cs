ec832eb05f8253db19d5a3291ceeca3c2305fda9
Vlad <VIStrelnikov94@gmail.com>
2022-12-10 02:28:50 +0300

    Добавил метод на сохранение и методы на добавление в группу

---
+++ SeparatorIntoGroup/SeparatorIntoGroup/ProjectCore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeparatorIntoGroup
+{
+    public class ProjectCore
+    {
+        public List<Student> Students { get; set; }
+        public List<Group> Groups { get; set; }
+        public List<Teacher> Teachers { get; set; }
+
+        public ProjectCore() 
+        { 
+            Students = new List<Student>();
+            Groups = new List<Group>();
+            Teachers= new List<Teacher>();
+        }
+    }
+}
